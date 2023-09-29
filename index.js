d0d9a3e2cf6885d92ba033a592bab80e999f8a21
KennyhSedera <kennyhsedera@gmail.com>
2023-09-29 20:56:06 +0300

    17 eme commit

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const { sequelize } = require('./dbConfig/db');
-const route = require('./routes/routes');
+// const route = require('./routes/routes');
 const app = express();
 const PORT = 3300;
 
+app.use('/', (req, res) => {
 sequelize.authenticate()
 .then(() => {
-    console.log('Connection a la base de donnees avec succees !');
+    res.send('Connection a la base de donnees avec succees !');
 }).catch((err) => {
     console.log(err);
-});
+});   
+})
 
-app.use(route)
+// app.use(route)
 
 
 app.listen(PORT, () => {
