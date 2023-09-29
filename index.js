bc9224571cbcef2e3b15c19a049c5c4bf6cfaca8
KennyhSedera <kennyhsedera@gmail.com>
2023-09-29 15:17:28 +0300

    8 eme commit

--- index.js
+++ index.js
@@ -1,40 +1,66 @@
 const express = require('express');
 const app = express();
 const mysql = require('mysql2');
+const Sequelize = require('sequelize');
+const PORT = 3500;
 
 const config = {
-    DB_HOST: 'kennyhsedera.alwaysdata.net',
+    // DB_HOST: 'kennyhsedera.alwaysdata.net',
     DB_USER: '327373_kennyh',
     DB_PASSWORD: 'sedera2020',
-    DB_NAME:'kennyhsedera_02'
+    DB_NAME: 'kennyhsedera_02',
+    Option: {
+        host: 'kennyhsedera.alwaysdata.net',
+        dialect: 'mysql',
+    }
 }
 
-const db = mysql.createConnection({
-    host: config.DB_HOST,
-    user: config.DB_USER,
-    password: config.DB_PASSWORD,
-    database: config.DB_NAME
-});
+// Connexion a la DB
+const sequelize = new Sequelize(
+    config.DB_NAME,
+    config.DB_USER,
+    config.DB_PASSWORD,
+    config.Option
+);
 
-db.connect((err) => {
-    if (err) {
-        console.log("Connection to the database failed!");
-    } else {
-        console.log("Connected to the database!");
-    }
-});
+// initialize();
 
-app.use('/', (req, res) => {
-     const sql = "SELECT * FROM `menu`"
-    db.query(sql, (err, result) => {
-        if (err) {
-            return res.send(err);
-        }
-        return res.send(result);
-    });
-})
+try {
+    sequelize.authenticate()
+    console.log('Connection a la base de donnees avec succees !');
+} catch (err) {
+    console.log('error');
+}
 
 
-app.listen(2000, (req, res)=>{
-    res.send('server running');
+// const db = mysql.createConnection({
+//     host: config.DB_HOST,
+//     user: config.DB_USER,
+//     password: config.DB_PASSWORD,
+//     database: config.DB_NAME
+// });
+
+// db.connect((err) => {
+//     if (err) {
+//         console.log("Connection to the database failed!");
+//     } else {
+//         console.log("Connected to the database!");
+//     }
+// });
+
+// app.use('/', (req, res) => {
+//      const sql = "SELECT * FROM `menu`"
+//     db.query(sql, (err, result) => {
+//         if (err) {
+//             return res.send(err);
+//         }
+//         return res.send(result);
+//     });
+// })
+
+
+sequelize.sync().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Serveur running sur le port: http://localhost:${PORT}`);
+    })
 })
