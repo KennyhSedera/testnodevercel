e8a8a81515d06ebabcf148439589cc325b4949cb
KennyhSedera <kennyhsedera@gmail.com>
2023-09-29 14:48:44 +0300

    third commit

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 const express = require('express');
 const app = express();
+const mysql = require('mysql2');
 
-app.use('/', (req, res) => {
-    res.send('Mandeha');
-})
+const config = {
+    DB_HOST: 'kennyhsedera.alwaysdata.net',
+    DB_USER: '327373_kennyh',
+    DB_PASSWORD: 'sedera2020',
+    DB_NAME:'kennyhsedera_02'
+}
+
+const db = mysql.createConnection({
+    host: config.DB_HOST,
+    user: config.DB_USER,
+    password: config.DB_PASSWORD,
+    database: config.DB_NAME
+});
+
+
+db.connect((err) => {
+    if (err) {
+        console.log("Connection to the database failed!");
+    } else {
+        app.use('/', (req, res) => {
+            res.send('Mandeha');
+        })
+        console.log("Connected to the database!");
+    }
+});
 
 app.listen(2000, ()=>{
     console.log('server running');
